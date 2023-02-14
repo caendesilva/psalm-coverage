eec967812f223ae9a333edd14bac9fb3391bdd6f
Caen De Silva <caen@desilva.se>
2023-02-14 15:30:31 +0100

    Construct Console instance

--- src/Psalm/Coverage/Reporters/ConsoleReporter.php
+++ src/Psalm/Coverage/Reporters/ConsoleReporter.php
@@ -4,15 +4,18 @@ declare(strict_types=1);
 
 namespace Psalm\Coverage\Reporters;
 
+use Desilva\Console\Console;
 use Psalm\Coverage\TypeCoverage;
 
 class ConsoleReporter implements TypeCoverageReportInterface
 {
     protected TypeCoverage $coverage;
+    protected Console $console;
 
     public function __construct(TypeCoverage $coverage)
     {
         $this->coverage = $coverage;
+        $this->console = new Console();
     }
 
     public function __invoke(): void
