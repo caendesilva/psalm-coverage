b4d9d83c701ac287b29ac81f6cbd67ceb7e084da
Caen De Silva <caen@desilva.se>
2023-02-14 15:43:23 +0100

    Display the formatted file information

--- src/Psalm/Coverage/Reporters/ConsoleReporter.php
+++ src/Psalm/Coverage/Reporters/ConsoleReporter.php
@@ -44,6 +44,9 @@ class ConsoleReporter implements TypeCoverageReportInterface
 
     protected function formatFileInfo(FileCoverageData $file): string
     {
-        //
+        $path_nonmixed_count = $file->getNonMixedCount();
+        $path_mixed_count = $file->getMixedCount();
+        $file_path = $file->getPath();
+        return str_pad($path_nonmixed_count ? str_pad(number_format((($path_nonmixed_count / ($path_mixed_count + $path_nonmixed_count)) * 100), 2), 5, '0', STR_PAD_LEFT)  : 'N/A', 6, ' ', STR_PAD_LEFT)." - File $file_path has $path_mixed_count mixed and $path_nonmixed_count non-mixed";
     }
 }
