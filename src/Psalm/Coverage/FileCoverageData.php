b31f189d68f4d0b4a08c7f17fa79301238f792e9
Caen De Silva <caen@desilva.se>
2023-02-14 15:11:58 +0100

    Add properties for the type counts

--- src/Psalm/Coverage/FileCoverageData.php
+++ src/Psalm/Coverage/FileCoverageData.php
@@ -7,6 +7,8 @@ namespace Psalm\Coverage;
 class FileCoverageData
 {
     protected string $path;
+    protected int $mixedCount;
+    protected int $nonMixedCount;
 
     public function __construct(string $path)
     {
