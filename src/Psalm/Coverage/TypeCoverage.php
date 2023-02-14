96e5fac5e6a81c469930e0f3f0dc54f99c03c1d4
Caen De Silva <caen@desilva.se>
2023-02-14 15:27:01 +0100

    Create reporters array

--- src/Psalm/Coverage/TypeCoverage.php
+++ src/Psalm/Coverage/TypeCoverage.php
@@ -4,8 +4,15 @@ declare(strict_types=1);
 
 namespace Psalm\Coverage;
 
+use Psalm\Coverage\Reporters\ConsoleReporter;
+
 class TypeCoverage
 {
+    /** @var array<class-string<\Psalm\Coverage\Reporters\TypeCoverageReportInterface>> */
+    public static array $reporters = [
+        ConsoleReporter::class,
+    ];
+
     protected static self $instance;
 
     /** @var array<string, FileCoverageData> */
